ec0feba7a82c46e8e14c7196f83ddd8c926f2bb7
Savio Fagundes <akirasfm@gmail.com>
2024-07-11 22:05:42 -0300

    Projeto Iniciado

---
+++ xadrez-console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace xadrez_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
