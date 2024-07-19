d3aaeec2d47fa6db3ae411038b4b9747a5844b80
Savio Fagundes <akirasfm@gmail.com>
2024-07-19 14:30:08 -0300

    Tratando erros com excecao personalizada

---
+++ xadrez-console/tabuleiro/TabuleiroExceptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.tabuleiro
+{
+    internal class TabuleiroExceptions
+    {
+    }
+}
