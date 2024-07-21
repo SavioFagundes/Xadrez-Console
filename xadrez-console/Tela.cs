7c79f7af9a305af14a25a39d3f31edbf71af2cf1
Savio Fagundes <akirasfm@gmail.com>
2024-07-21 20:20:23 -0300

    Implementando a logica de xeque mate

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -1,11 +1,56 @@
 ﻿using tabuleiro;
+using xadrez;
 using xadrez_console.tabuleiro;
 using xadrez_console.xadrez;
+using System.Collections.Generic;
 
 namespace xadrez_console
 {
     internal class Tela
     {
+        public static void imprimirPartida(PartidaDeXadrez partida)
+        {
+            Tela.imprimirTabuleiro(partida.tab);
+            Console.WriteLine();
+            imprimirPecasCapturadas(partida);
+            Console.WriteLine();
+            Console.WriteLine("Turno" + partida.Turno);
+            if (!partida.terminada)
+            {
+                Console.WriteLine("Aguardando jogada da partida: " + partida.JogadorAtual);
+                if (partida.Xeque)
+                {
+                    Console.WriteLine("Xeque!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Xeque Mate");
+                Console.WriteLine("Vencedor: " + partida.JogadorAtual);
+            }
+        }
+        public static void imprimirPecasCapturadas(PartidaDeXadrez partida)
+        {
+            Console.WriteLine("Peças capturadas:");
+            Console.Write("Brancas");
+            imprimirConjunto(partida.pecasCapturadas(Cor.Branco));
+            Console.WriteLine();
+            Console.Write("Pretas");
+            ConsoleColor aux = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            imprimirConjunto(partida.pecasCapturadas(Cor.Preta));
+            Console.ForegroundColor = aux;
+            Console.WriteLine();
+        }
+        public static void imprimirConjunto(List<Peca> conjunto)
+        {
+            Console.WriteLine("[");
+            foreach (Peca x in conjunto)
+            {
+                Console.WriteLine(x + " ");
+            }
+            Console.WriteLine("]");
+        }
         public static void imprimirTabuleiro(Tabuleiro tab)
         {
             for (int i = 0; i < tab.linhas; i++)
